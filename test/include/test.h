a48d29141f1824e25aecef78f408a8a020fcf67d
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2022-10-31 10:35:09 +0100

    code cleanup

--- test/include/test.h
+++ test/include/test.h
@@ -2,27 +2,24 @@
 #define TEST_TEST_H
 
 #include <kernel/com.h>
+#include <stdbool.h>
 
-#define FAIL {serial_writestring("Test terminated\n"); return -2;}
-#define PASS {serial_writestring("Test passed\n"); return 0;}
+#define FAIL {serial_writestring("Test terminated\n"); *(error_count)++; return;}
+#define PASS {serial_writestring("Test passed\n"); *(error_count) = 0; return;}
 
-void print_bad_assertion(int x, int y){
-		serial_writestring("Expected "); // TEMP to be replaced with sprintf
-}
+#define ASSERT(x, y) if (x == y) {} \
+	else { serial_writestring("Assert fail on "); serial_writestring(__FILE__); /* serial_writestring(__LINE__); to be replaced with sprintf*/ (*error_count)++; return;}
+#define EXPECT(x, y) if (x == y) {} \
+	else { serial_writestring("Assert fail on "); serial_writestring(__FILE__); /* serial_writestring(__LINE__); to be replaced with sprintf*/ (*error_count)++;}
 
-int assert(int x, int y){
-	return x == y;
-}
-
-#define ASSERT(x, y) if (x == y) {} else { print_bad_assertion(x,y); return -1;}
-#define EXPECT(x, y) if (x == y) {} else { print_bad_assertion(x,y); (*error_count)++;}
-
+#define ASSERT_TRUE(x) ASSERT(x, true);
+#define EXPECT_TRUE(x) EXPECT(x, true);
 
 #define TEST(x) void x##T(int*); int x() { int ec = 0; x##T(&ec); return ec;} void x##T(int* error_count)
 
 #define RUN(x) serial_writestring("Started: "); serial_writestring(#x); serial_writestring("\n"); \
 	if(x() == 0) { serial_writestring(#x); serial_writestring("[PASS]\n");}\ 
- else {serial_writestring(#x); serial_writestring("[FAIL]\n");}\
+	else {serial_writestring(#x); serial_writestring("[FAIL]\n");}\
 
 
 
