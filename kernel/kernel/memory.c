bea5fe279e0bf932d509aa72183cb19bf898eb4b
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2022-11-14 12:00:10 +0100

    added memeoty header

--- kernel/kernel/memory.c
+++ kernel/kernel/memory.c
@@ -8,5 +8,26 @@ struct{
 void init_memory_manager(void* base, size_t length){
 	available_memory.base = base;
 	available_memory.length = length;
+
+	memory_descriptor_t* desc = available_memory.base;
+	desc->size = available_memory.length;
+	desc->flags = 0;
 }
 
+void* allocate_next(memory_descriptor_t* desc, size_t length){
+
+}
+
+void deallocate(memory_descriptor_t* desc){
+	desc &= !USED_FLAG;
+	
+}
+
+memory_descriptor_t* get_initial_memory_descriptor(){
+	return available_memory.base;
+
+}
+
+size_t get_available_memory(){
+	return available_memory.length - available_memory.base;
+}
