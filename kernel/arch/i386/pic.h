94310734d5909b4e45cd025b95967404e8e79a5b
Michał Soja <m.sojka99@gmail.com>
2022-12-10 20:00:06 +0100

    compiling now

--- kernel/arch/i386/pic.h
+++ kernel/arch/i386/pic.h
@@ -25,6 +25,13 @@
 #define PIC2_COMMAND	PIC2
 #define PIC2_DATA	(PIC2+1)
 
+#define PIC_MASTER          0x20
+#define PIC_SLAVE		    0xA0
+#define PIC_MASTER_COMMAND	PIC_MASTER
+#define PIC_MASTER_DATA	    (PIC_MASTER+1)
+#define PIC_SLAVE_COMMAND	PIC_SLAVE
+#define PIC_SLAVE_DATA	    (PIC_SLAVE+1)
+#define PIC_EOI		        0x20
 
 
 /*
