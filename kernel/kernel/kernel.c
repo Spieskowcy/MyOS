45ae2c9089e65845be84d01f4bf4fce9a7fe1b19
Michał Soja <m.sojka99@gmail.com>
2022-12-13 16:02:14 +0100

    operand size mismatch for int

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+void func(void){
+    printf("text");
+}
 
 void kernel_main(void) {
+    isr_register_handler(0x81, func);
 	terminal_initialize();
+    __asm__ volatile ("int 81h");
 	int v = 12;
 	int a = 0xF, b = 0xF0000000;
 	int* wsk = &v; 
